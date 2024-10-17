5c2eaf674bfce8a56a3d139ad9465f200d711e69
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-17 13:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// Complete the js code
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+    this.make = make;
+    this.model = model;
 }
-Car.prototype.getMakeModel=function(){
-	return `${this.make} ${this.model}`;
+
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
 }
 
 function SportsCar(make, model, topSpeed) {
-  Car.call(this,make,model);
-	this.topSpeed=topSpeed;
+    Car.call(this, make, model);  // Inherit properties from Car
+    this.topSpeed = topSpeed;
 }
-SportsCar.prototype=object.create(Car.prototype);
-SportsCar.prototype.constructor=SportsCar;
 
-SportsCar.prototype.getTopSpeed=function(){
-	return this.topSpeed;
+// Inherit from Car
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Correct the constructor property
+SportsCar.prototype.constructor = SportsCar; 
+
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
 }
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
